2fbc7d3ed56bc00f15827b6efa4dedc8c6251395
wr-sky <90881835+wr-sky@users.noreply.github.com>
2022-04-06 22:14:38 +0800

    Add files via upload

--- 1-download/gunzip.py
+++ 1-download/gunzip.py
@@ -2,12 +2,10 @@ import pandas as pd
 import os
 import glob
 
-#for target in ['fna', 'faa']:
-for target in ['faa']:
+for target in ['fna', 'faa']:
     out_dir = "/home/wbq/1/Bacteria/Ref"
     faa_files = sorted(glob.glob(os.path.join(out_dir, target, '*')))
     for i in faa_files:
         print(i)
         cmd = "gunzip -c " + i + " > " + out_dir + "/" + target + "_gunzip/" + i.split('/')[7].split('_')[1].split('.')[0] + "." + target
         os.system(cmd)
-#001941465
